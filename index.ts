7fa2c4321ec668b3ec431bebbd4e0cac0f2b48a0
Uzmakanwl <154093505+Uzmakanwl@users.noreply.github.com>
2024-03-23 21:26:34 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    {message:"Enter secound number",type:"number",name:"secoundNumber" },
+    { 
+     message:"select one of the opreaters to perform operation ",
+     type:"list",
+     name:"operator",
+     choices:["Addition","Subtraction","Multiplicaton","Division"],
+    },
+]);
+if(answer.operater === "Addition"){
+    console.log(answer.firstNumber + answer.secondNumber);
+} else if(answer.operater === "Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber);
+}else if(answer.operater === "Multiplicaton"){
+    console.log(answer.firstNumber * answer.secondNumber);
+}else if(answer.operater === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber);
+}else{
+    console.log("Please Select Valid Operation.");
+}
